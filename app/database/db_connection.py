9e7a57c32dc7d000cfa8c4ef63cca8b2e9ad2693
Yoni <yonilev2021@gmail.com>
2026-06-11 12:14:08 +0300

    implemented create_tables

--- app/database/db_connection.py
+++ app/database/db_connection.py
@@ -8,7 +8,31 @@ def get_connection():
         database='library_db'
     )
 
-conn = get_connection()
-cur = conn.cursor()
-cur.execute("SHOW DATABASES")
-print("Databases:", [r[0] for r in cur.fetchall()])
+
+def create_tables():
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("""
+        CREATE TABLE member (
+            id INT AUTO_INCREMENT PRIMARY KEY, 
+            email VARCHAR(255) NOT NULL UNIQUE, 
+            name VARCHAR(50) NOT NULL ,
+            is_active BOOLEAN NOT NULL,
+            total_borrow INT NOT NULL
+            ); 
+        """)
+    cursor.close()
+    cursor = conn.cursor()
+    cursor.execute("""
+        CREATE TABLE book (
+            id INT AUTO_INCREMENT PRIMARY KEY, 
+            title VARCHAR(50) NOT NULL, 
+            author VARCHAR(50) NOT NULL ,
+            genre ENUM('Fiction', 'Non-Fiction', 'Science', 'History', 'Other') NOT NULL,
+            is_available BOOLEAN NOT NULL,
+            borrowed_by_member_id INT
+            ); 
+        """)
+    cursor.close()
+    conn.close()
+    
