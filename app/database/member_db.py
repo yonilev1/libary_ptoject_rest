53110dae5f974a7d21a3f2042ad4a58562fdd60b
Yoni <yonilev2021@gmail.com>
2026-06-12 10:35:34 +0300

    implemented create_member and email_exists

---
+++ app/database/member_db.py
@@ -0,0 +1,47 @@
+from db_connection import get_connection
+
+class MemeberDb:
+    def create_member(self, data):
+        """
+        create a member and store in db
+
+        returns: number of new row or None if didnt add
+
+        raises: valueerror if genre mot valid
+        """
+        conn = get_connection()
+        cursor = conn.cursor()
+
+        if self.email_exists(data['email']):
+            raise ValueError('email already exists')
+
+        cursor.execute("""
+        INSERT INTO member (email, name, is_active, total_borrow)
+        VALUES(%s, %s, True, 0);
+        """, (data['email'], data['name']))
+        conn.commit()
+        did_add = cursor.lastrowid
+        cursor.close()
+        conn.close()
+        return did_add
+    
+    def email_exists(self, email):
+        """
+        check if email exist
+        """
+        conn = get_connection()
+        cursor = conn.cursor()
+        cursor.execute("""
+        SELECT email FROM member WHERE email = %s;
+        """, (email,))
+        row = cursor.fetchone()
+        cursor.close()
+        conn.close()
+        return True if row is not None else False
+    
+
+
+
+member = MemeberDb()
+member.create_member({'name':'Yoni', 'email':'yoni@gmail.com'})
+member.create_member({'name':'Yoni', 'email':'yoni@gmail.com'})
