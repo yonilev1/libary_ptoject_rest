659fc8930e275d6fe590794cdb8a925466205ad3
Yoni <yonilev2021@gmail.com>
2026-06-12 17:06:11 +0300

    fix borrow book logic

--- routes/book_routes.py
+++ routes/book_routes.py
@@ -61,15 +61,21 @@ def update_book(id:int, book:UpdateBook = Query(...)):
 def borrow_book(id:int, member_id:int):
     new_book = book_db.BookDB()
     new_member = member_db.MemeberDb()
-    if new_member.count_borrowes(member_id) == 3:
-        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='cant borrow more then 3 books')
-    try:
-        if new_member.member_exists(member_id):
-            updated_book = new_book.set_available(id, 0, member_id)
-    except Exception as e:
-        raise HTTPException(status.HTTP_400_BAD_REQUEST, detail=str(e))
-    if updated_book == 0:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+    if new_member.get_member_by_id(member_id):
+        if new_book.get_book_by_id(id):
+            if new_member.count_borrowes(member_id) < 3:
+                try:
+                    lend_book = new_book.set_available(id, 0, member_id)
+                except Exception as e:
+                    raise HTTPException(status.HTTP_400_BAD_REQUEST, detail=str(e))
+                if lend_book == 0:
+                    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+                else:
+                    new_member.increment_borrows(member_id)
+                    return 'book borrowed successfully'
+            else:
+                raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='cant borrow more then 3 books')
+        else:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'book with id {id} does not exist')
     else:
-        new_member.increment_borrows(member_id)
-        return 'book borrowed successfully'
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f'member with id {member_id} does not exist')
