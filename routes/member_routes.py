aac9f22fc10ab5c37fcf77a5052e715f82366d39
Yoni <yonilev2021@gmail.com>
2026-06-13 21:57:37 +0300

    implement get to get all members and get member by id

--- routes/member_routes.py
+++ routes/member_routes.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter,status, HTTPException, Query
-from database import book_db, member_db
+from database import member_db, member_db
 from pydantic import BaseModel
 
 class CreateMember(BaseModel):
@@ -24,3 +24,18 @@ def create_member(member : CreateMember):
             raise HTTPException(status.HTTP_400_BAD_REQUEST)
     except ValueError as e:
         raise HTTPException(status.HTTP_400_BAD_REQUEST, detail=str(e))
+    
+
+@router.get('/members')
+def get_all_members():
+    new_member = member_db.MemeberDb()
+    return new_member.get_all_members()
+
+
+@router.get('/members/{id}')
+def get_member_by_id(id:int):
+    new_member = member_db.MemeberDb()
+    get_member = new_member.get_member_by_id(id)
+    if not get_member:
+        raise HTTPException(status.HTTP_404_NOT_FOUND)
+    return get_member
