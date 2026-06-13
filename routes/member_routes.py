6927db4205db5d146427d95b77afc5fe44605ca7
Yoni <yonilev2021@gmail.com>
2026-06-13 22:25:37 +0300

    implement put to deactivate  member

--- routes/member_routes.py
+++ routes/member_routes.py
@@ -9,7 +9,7 @@ class CreateMember(BaseModel):
 class UpdateMember(BaseModel):
     name:str | None = None
     email:str | None = None
-    is_active:str | None = None
+    is_active:bool | None = None
     total_borrow:int | None = None
 
 router = APIRouter()
@@ -50,6 +50,18 @@ def update_member(id:int, member:UpdateMember = Query(...)):
     except Exception as e:
         raise HTTPException(status.HTTP_400_BAD_REQUEST, detail=str(e))
     if updated_member == 0:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail='user already deactivated')
+    elif updated_member == -1:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
     else:
         return 'member updated successfully'
+    
+
+
+@router.put('/members/{id}/deactivate')
+def deactivate_member(id:int):
+    try:
+        deactive = update_member(id, UpdateMember(is_active=False))
+    except Exception as e:
+        raise
+    return 'member deactivated successfully'
