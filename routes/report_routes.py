9065b9264079d69773143d76ac8e8ddf4ea925e7
Yoni <yonilev2021@gmail.com>
2026-06-14 09:49:29 +0300

    implement get to get summary

---
+++ routes/report_routes.py
@@ -0,0 +1,14 @@
+from fastapi import APIRouter,status, HTTPException, Query
+from database import book_db, member_db
+from pydantic import BaseModel
+
+router = APIRouter()
+
+@router.get('/reports/summary')
+def get_summery():
+    new_book = book_db.BookDB()
+    new_member = member_db.MemeberDb()
+    total_books = new_book.count_total_books()
+    available_books = new_book.count_available_books()
+    active_members = new_member.count_active_members()
+    return {"total_books": total_books, "available_books": available_books, "currently_borrowed": total_books - available_books, "active_members": active_members}
